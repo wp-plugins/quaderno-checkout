768c1dee37951f720186f613433fbbfb484989ee
polimorfico <polimorfico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 19:55:48 +0000

    New version
    git-svn-id: https://plugins.svn.wordpress.org/quaderno-checkout/tags/1.2.1@1217541 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/shortcodes.php
@@ -0,0 +1,63 @@
+<?php 
+
+/**
+* Shortcodes
+*
+* @package    Quaderno
+* @copyright  Copyright (c) 2015, Quaderno
+* @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+* @since      1.0.0
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function quaderno_checkout_shortcode( $attr, $content = null ) {
+  extract(shortcode_atts(array(
+  'type' => 'charge',
+  'gateway' => 'stripe',
+  'amount' => 500,
+  'currency' => 'USD',
+  'plan' => '',
+  'transaction_type' => 'eservice',
+  'description' => 'Checkout',
+  'label' => 'Pay Now',
+  'panel_label' => 'Pay {{amount}}',
+  'locale' => 'en',
+  'billing_address' => false,
+  'redirect_url' => ''
+  ), $attr));
+
+  if ( $type == 'charge' ) {
+    $token = array(
+      'iat' => time(),
+      'amount' => $amount,
+      'currency' => $currency,
+      'description' => $description,
+      'transaction-type' => $transaction_type
+    );
+
+    $transaction_info = 'data-charge="' . JWT::encode($token, get_option('quaderno_private_key')) . '"';
+  } else {
+    $transaction_info = 'data-plan="' . $plan . '"';    
+  }
+
+  $html = '<form action="' . $redirect_url . '" method="POST">
+    <script
+    src="https://checkout.quaderno.io/checkout.js" class="quaderno-button"
+  data-key="' . get_option('quaderno_public_key') . '"
+    data-type="' . $type . '"
+    data-gateway="' . $gateway . '"
+    data-description="' . $description . '"
+    data-amount="' . $amount . '"
+    data-currency="' . $currency . '"
+    data-transaction-type="' . $transaction_type . '"
+    data-label="' . $label . '"
+    data-panel-label="'. $panel_label . '"
+    data-locale="' . $locale . '"
+    data-billing-address="' . $billing_address . '" ' . $transaction_info . '></script>
+  </form>';
+
+  return $html;
+}
+add_shortcode('quaderno', 'quaderno_checkout_shortcode');
